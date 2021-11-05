b44ac4f65ccfcf962d14a879eb532e1e93ad53b8
Kanishka Samarathunga <kanishkakcs@gmail.com>
2021-11-05 07:32:06 +0530

    Components Part-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponents";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
 }
 
 export default App;
